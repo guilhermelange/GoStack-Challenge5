19daca560257039938ceb0fd2d245bdb80c7a132
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2021-09-27 00:07:51 -0300

    Initial commit

---
+++ src/routes/transaction.routes.ts
@@ -0,0 +1,45 @@
+import { Router } from 'express';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+const transactionRouter = Router();
+
+const transactionsRepository = new TransactionsRepository();
+
+transactionRouter.get('/', (request, response) => {
+  try {
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    });
+  } catch (_err) {
+    const err = _err as Error;
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+transactionRouter.post('/', (request, response) => {
+  try {
+    const { title, value, type } = request.body;
+
+    const CreateTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = CreateTransaction.execute({
+      title,
+      value,
+      type,
+    });
+    return response.json(transaction);
+  } catch (_err) {
+    const err = _err as Error;
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default transactionRouter;
